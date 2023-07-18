b88399435f617ee3253d19a1ad34b9af3b51effe
Ralf Konrad <42419984+ralfkonrad@users.noreply.github.com>
2023-07-18 19:27:41 +0200

    Updated EuropeanOptionTest

--- src/test/java/io/github/ralfkonrad/EuropeanOptionTest.java
+++ src/test/java/io/github/ralfkonrad/EuropeanOptionTest.java
@@ -2,42 +2,54 @@ package io.github.ralfkonrad;
 
 import org.junit.jupiter.api.Test;
 import org.quantlib.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
 
 public class EuropeanOptionTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EuropeanOptionTest.class);
+
     @Test
     public void testEuropeanOption() {
-        Settings.instance().setEvaluationDate(new Date(14, Month.July, 2023));
-        var today = Settings.instance().getEvaluationDate();
+        var localDate = LocalDate.of(2023, 7, 28);
+        var today = Date.of(localDate);
+        Settings.instance().setEvaluationDate(today);
 
+        // Set up the option parameters
+        var strikePrice = 105.0;
+        var maturityDate = LocalDate.of(2023, 12, 31);
+
+        // Create the option objects
+        var payoff = new PlainVanillaPayoff(Option.Type.Call, strikePrice);
+        var exercise = new EuropeanExercise(Date.of(maturityDate));
+        var europeanOption = new EuropeanOption(payoff, exercise);
+
+        // Create the market data object
         var dayCounter = new Actual360();
         var calendar = new NullCalendar();
 
-        // Set up the option parameters
         var spotPrice = 100.0;
-        var strikePrice = 105.0;
+        var spotPriceHandle = new QuoteHandle(new SimpleQuote(spotPrice));
+
         var riskFreeRate = 0.05;
+        var riskFreeRateHandle = new QuoteHandle(new SimpleQuote(riskFreeRate));
+
         var volatility = 0.2;
-        var maturityDate = new Date(31, Month.December, 2023);
+        var volatilityHandle = new QuoteHandle(new SimpleQuote(volatility));
 
-        // Create the option objects
-        var spot = new QuoteHandle(new SimpleQuote(spotPrice));
-        var rate = new QuoteHandle(new SimpleQuote(riskFreeRate));
-        var vol = new QuoteHandle(new SimpleQuote(volatility));
-        var yieldCurve = new YieldTermStructureHandle(new FlatForward(today, rate, dayCounter));
-        var vola = new BlackVolTermStructureHandle(new BlackConstantVol(today, calendar, vol, dayCounter));
+        var yieldTermStructure = new YieldTermStructureHandle(new FlatForward(today, riskFreeRateHandle, dayCounter));
+        var blackVolTermStructure = new BlackVolTermStructureHandle(new BlackConstantVol(today, calendar, volatilityHandle, dayCounter));
 
-        var payoff = new PlainVanillaPayoff(Option.Type.Call, strikePrice);
-        var exercise = new EuropeanExercise(maturityDate);
-        var europeanOption = new EuropeanOption(payoff, exercise);
-
-        // Calculate the option price
-        var process = new BlackScholesProcess(spot, yieldCurve, vola);
+        // Create the AnalyticEuropeanEngine
+        var process = new BlackScholesProcess(spotPriceHandle, yieldTermStructure, blackVolTermStructure);
         var engine = new AnalyticEuropeanEngine(process);
-        europeanOption.setPricingEngine(engine);
 
+        // Calculate the NPV for the European option
+        europeanOption.setPricingEngine(engine);
         var npv = europeanOption.NPV();
 
         // Print the result
-        System.out.println("Option price: " + npv);
+        LOGGER.info("Option price: {}", npv);
     }
 }
